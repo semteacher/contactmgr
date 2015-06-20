9e97bcdd7bd93b599ae77cf4324881a1e7e1eb15
semteacher <semteacher@gmail.com>
2015-06-20 21:08:47 +0300

    create app skeleton

---
+++ app/views/pages/home.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SemenetsA
+ * Date: 20.06.2015
+ * Time: 16:55
+ */
+
+?>
+
+<p>Hello there <?php echo $first_name . ' ' . $last_name; ?>!<p>
+
+<p>You successfully landed on the home page. Congrats!</p>
