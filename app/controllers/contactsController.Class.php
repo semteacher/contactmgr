03c0507c4c9c5504ab13022ee95758978e1176eb
semteacher <semteacher@gmail.com>
2015-06-30 20:24:02 +0300

    move to more objects in index action

--- app/controllers/contactsController.Class.php
+++ app/controllers/contactsController.Class.php
@@ -7,6 +7,7 @@
  */
 
 class ContactsController extends Controller {
+
     public function __construct($model, $action)
     {
         parent::__construct($model, $action);
@@ -57,7 +58,85 @@ class ContactsController extends Controller {
 
     public function save()
     {
+        if (!isset($_POST['editcontactsubmit']))
+        {
+            header('Location: /contact/index');
+        } elseif ($_POST['editcontactsubmit']=='cancel'){
+            header('Location: /contact/index');
+        }
+        $errors = array();
+        $check = true;
 
+        //get POST data
+        $firstName = isset($_POST['fname']) ? trim($_POST['fname']) : NULL;
+        $lastName = isset($_POST['lname']) ? trim($_POST['lname']) : NULL;
+        $email = isset($_POST['email']) ? trim($_POST['email']) : "";
+        $phoneHome = isset($_POST['phone_h']) ? trim($_POST['phone_h']) : "0000";
+
+        //form data validation
+        if (empty($email))
+        {
+            $check = false;
+            array_push($errors, "E-mail is required!");
+        }
+//        else if (!filter_var( $email, FILTER_VALIDATE_EMAIL ))
+//        {
+//            $check = false;
+//            array_push($errors, "Invalid E-mail!");
+//        }
+
+        if (empty($phoneHome))
+        {
+            $check = false;
+            array_push($errors, "At least one phone is required!");
+        }
+
+        //form data is invalid
+        if (!$check)
+        {
+            $this->_setView('edit');
+            $this->_view->set('title', 'Contact Details Form - Error');
+            $this->_view->set('pageheader', 'Contact Details - Invalid contact form data!');
+            $this->_view->set('errors', $errors);
+            $this->_view->set('contact', $_POST);
+            return $this->_view->output();
+        }
+
+        //store correct data
+        try {
+            $contact = $_POST['mode'] == 'add' ? new ContactModel() : $this->_model->getContactById((int)$_POST['id_contact']);
+            $contact->setFirstName($firstName);
+            $contact->setLastName($lastName);
+            $contact->setEmail($email);
+            $contact->setPhoneHome($phoneHome);
+
+            $_POST['mode'] == 'add' ? $contact->add($contact) : $contact->update($contact);
+
+
+            $this->_setView('index');
+            $this->_view->set('title', 'BundleJoy - Contact Manager');
+            $this->_view->set('pageheader', 'Contact Management Main Page - store success!');
+
+//            $data = array(
+//                'firstName' => $firstName,
+//                'lastName' => $lastName,
+//                'email' => $email,
+//                'message' => $phoneHome
+//            );
+//
+//            $this->_view->set('userData', $data);
+
+            $this->_view->set('contact', $contact);
+
+        } catch (Exception $e) {
+            $this->_setView('edit');
+            $this->_view->set('title', 'Contact Details Form - Error');
+            $this->_view->set('pageheader', 'Contact Details - There was an error saving the data!');
+            $this->_view->set('contact', $_POST);
+            $this->_view->set('saveError', $e->getMessage());
+        }
+
+        return $this->_view->output();
     }
 
     public function del()
--- app/models/contactsModel.Class.php
+++ app/models/contactsModel.Class.php
@@ -7,9 +7,87 @@
  */
 
 class ContactsModel extends Model {
+    private $_idContact;
+    private $_firstName;
+    private $_lastName;
+    private $_email;
+    private $_phoneHome;
+
+//    public function __construct()
+//    {
+//        parent::__construct();
+//    }
+    /**
+     * @param mixed $idContact
+     */
+    public function setIdContact($idContact)
+    {
+        $this->_idContact = $idContact;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdContact()
+    {
+        return $this->_idContact;
+    }
+
+    public function setFirstName($firstName)
+    {
+        $this->_firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->_firstName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->_lastName = $lastName;
+    }
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->_lastName;
+    }
+
+    public function setEmail($email)
+    {
+        $this->_email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->_email;
+    }
+
+    public function setPhoneHome($phoneHome)
+    {
+        $this->_phoneHome = $phoneHome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoneHome()
+    {
+        return $this->_phoneHome;
+    }
 
     public function getAllContacts()
     {
+        $contactlist = [];
+
         $sql = "SELECT
                     *
                 FROM
@@ -17,13 +95,24 @@ class ContactsModel extends Model {
 
         $this->_setSql($sql);
         $contacts = $this->getAll();
-
+var_dump($contacts);
         if (empty($contacts))
         {
             return false;
+        } else {
+            foreach ($contacts as $contact) {
+                $tmpcontact = new ContactsModel;
+                $tmpcontact->setIdContact($contact['id_contact']);
+                $tmpcontact->setFirstName($contact['fname']);
+                $tmpcontact->setLastName($contact['lname']);
+                $tmpcontact->setEmail($contact['email']);
+                array_push($contactlist, $tmpcontact);
+            }
+            var_dump($contactlist);
+            return $contactlist;
         }
 
-        return $contacts;
+
     }
 
     public function getContactById($id)
