daac3d58aaa7fa39057564a36c1167ca4be28473
semteacher <semteacher@gmail.com>
2015-07-06 10:49:04 +0300

    add display fields on contact index page

--- app/views/contacts/index.php
+++ app/views/contacts/index.php
@@ -26,7 +26,7 @@
 </div>
 
     <div>
-        <span>First</span><span>Last</span><span>E-mail</span><span>Best phone</span>
+        <span>First</span><span>Last</span><span>E-mail</span><span>Best phone</span><span>Address 1</span><span>Address 2</span><span>City</span><span>State</span><span>Country</span><span>Zip</span><span>Birthday</span>
     </div>
 
 <?php if ($contacts): foreach ($contacts as $contact):  ?>
@@ -36,6 +36,15 @@
         <span><?php echo $contact->getLastName(); ?></span>
         <span><?php echo $contact->getEmail(); ?></span>
         <span><?php echo $contact->getPhoneHome(); ?></span>
+        <span><?php echo $contact->getPhoneWork(); ?></span>
+        <span><?php echo $contact->getPhoneCell(); ?></span>
+        <span><?php echo $contact->getAddress1(); ?></span>
+        <span><?php echo $contact->getAddress2(); ?></span>
+        <span><?php echo $contact->getCity(); ?></span>
+        <span><?php echo $contact->getState(); ?></span>
+        <span><?php echo $contact->getCountry(); ?></span>
+        <span><?php echo $contact->getZip(); ?></span>
+        <span><?php echo $contact->getBirthday(); ?></span>
         <span><a href="/contacts/edit/<?php echo $contact->getIdContact(); ?>">Edit/View</a></span>
         <span><a href="/contacts/del/<?php echo $contact->getIdContact(); ?>" onclick="return confirm('Are you sure you want to delete?')">Delete</a></span>
     </div>
